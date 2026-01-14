5fe1b18f52d6f9f57baec7fc83534718b5ff2ad2
Firestar-Reimu <1900011604@pku.edu.cn>
2026-01-14 16:26:38 +0800

    HI4PI HVC compare

--- Galaxy Rotation Model/v_dev_30.sh
+++ Galaxy Rotation Model/v_dev_30.sh
@@ -1,1 +1,2 @@
-python process_and_mask_cube_numpy.py ../CRAFTS/South/Combined/*.fits --model=poly --v_dev=30
+# python process_and_mask_cube_numpy.py ../CRAFTS/South/Combined/*.fits --model=poly --v_dev=30
+python process_and_mask_cube_numpy.py ../HI4PI/HI4PI_RA*.fits --model=poly --v_dev=30
